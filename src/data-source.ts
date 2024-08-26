cd0f444b4bc24e311094956bf1ae06612aacf38c
jfct <jorgecosteixeira@gmail.com>
2024-08-26 21:18:50 +0100

    fix: improvements to data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,8 +1,5 @@
 import * as dotenv from "dotenv";
 import { DataSource } from "typeorm";
-import { File } from "./entity/file.entity";
-import { Group } from "./entity/group.entity";
-import { User } from "./entity/user.entity";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -14,9 +11,9 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER || "admin",
     password: process.env.DB_PASSWORD || "admin",
     database: process.env.DB_NAME || "test",
-    synchronize: true,
-    logging: true,
-    entities: [File, Group, User],
-    migrations: ['src/migration/*.ts'],
+    synchronize: false,
+    logging: false,
+    entities: [(process.env.ENV == 'local' ? 'src/entity/*.ts' : 'dist/entity/*.js')],
+    migrations: [(process.env.ENV == 'local' ? 'src/migration/*.ts' : 'dist/migration/*.js')],
     subscribers: [],
 })
